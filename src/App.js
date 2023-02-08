fac3681abdd78669d02176ac548fdd206832716a
Sachin Wariyal <sachinwariyal4@gmail.com>
2023-02-08 21:14:27 +0530

    removed exact path

--- src/App.js
+++ src/App.js
@@ -18,12 +18,12 @@ export default class App extends Component {
         {/* <News/> */}
         <Switch>
           <Route exact path="/Taaza-Khabar-NEWS"> <News key="general" pageSize={6} country ="in" category ="general" /> </Route>
-          <Route exact path="/bussiness"> <News key="bussiness" pageSize={6} country = "in" category ="bussiness" /> </Route>
-          <Route exact path="/entertainment"> <News key="entertainment" pageSize={6} country = "in" category ="entertainment" /> </Route>
-          <Route exact path="/sports"> <News key="sports" pageSize={6} country = 'in' category ='sports' /> </Route>
-          <Route exact path="/technology"> <News key="technology" pageSize={6} country = 'in' category ='technology' /> </Route>
-          <Route exact path="/health"> <News key="health" pageSize={6} country = 'in' category ='health' /> </Route>
-          <Route exact path="/science"> <News key="science" pageSize={6} country = 'in' category ='science' /> </Route>
+          <Route path="/bussiness"> <News key="bussiness" pageSize={6} country = "in" category ="bussiness" /> </Route>
+          <Route path="/entertainment"> <News key="entertainment" pageSize={6} country = "in" category ="entertainment" /> </Route>
+          <Route path="/sports"> <News key="sports" pageSize={6} country = 'in' category ='sports' /> </Route>
+          <Route path="/technology"> <News key="technology" pageSize={6} country = 'in' category ='technology' /> </Route>
+          <Route path="/health"> <News key="health" pageSize={6} country = 'in' category ='health' /> </Route>
+          <Route path="/science"> <News key="science" pageSize={6} country = 'in' category ='science' /> </Route>
         </Switch>
         </Router>
       </div>
